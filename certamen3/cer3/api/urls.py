5a8f142ececa29d7b8a534b89c4e0542c371b2eb
lagoslakes <143352007+sebastianlagoslakes@users.noreply.github.com>
2023-11-28 00:44:49 -0300

    Update urls.py

--- certamen3/cer3/api/urls.py
+++ certamen3/cer3/api/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path,include
+from rest_framework import routers 
+from api import views 
 
+router = routers.DefaultRouter()
+router.register(r'evento',views.EventoViewSet)
+
+urlpatterns=[
+  path('',include(router.urls))
+]
