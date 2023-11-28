6600c29cd77e8728759280620d292309cbbea910
Bastian B <bastian.berrios@sansano.usm.cl>
2023-11-27 22:42:24 -0300

    added views
    + ignore

--- certamen3/cer3/miapp/views.py
+++ certamen3/cer3/miapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    title = "Inicio"
+
+    data = {
+        "title": title,
+    }
+
+    return render(request,'cer3/inicio.html', data)
