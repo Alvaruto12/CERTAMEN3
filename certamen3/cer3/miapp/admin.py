8873818de4e49691634e122393c03d5ec87a43bc
lagoslakes <143352007+sebastianlagoslakes@users.noreply.github.com>
2023-11-27 22:20:44 -0300

    Update admin.py

--- certamen3/cer3/miapp/admin.py
+++ certamen3/cer3/miapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Evento, Segmento, Tipo
 
 # Register your models here.
+admin.site.register(Tipo) 
+admin.site.register(Evento) 
+admin.site.register(Segmento) 
